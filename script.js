c9cb5cc330db9dbd0b8ca110e03ad2db04a11025
J1741 <84889065+J1741@users.noreply.github.com>
2021-07-05 22:45:44 -0400

    adding current day to header

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-// testing moment
-var today = moment();
-console.log(today);
+/**
+ * globals
+*/
 
-// testing jquery
-var headerEl = $('<h1>');
-console.log(headerEl);
+// save references to important DOM elements
+var currentDayEl = $('#currentDay');
+
+/**
+ * planner header
+*/
+
+// handle displaying the current day
+function displayDay() {
+  // create and format var for current day
+  var today = moment().format('dddd[,] MMMM Do');
+  console.log(today);
+  // add current day to page header 
+  currentDayEl.text(today);
+
+};
+
+displayDay();
