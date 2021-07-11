eb44771359727d59ad1bb48683e6e841df71ed68
J1741 <84889065+J1741@users.noreply.github.com>
2021-07-10 22:48:06 -0400

    adding function to check current hour

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-/**
+/*
  * globals
-*/
+ */
 
 // save references to important DOM elements
 var currentDayEl = $('#currentDay');
 
-/**
+// set up global variables
+var currentHour;
+
+
+/*
  * planner header
-*/
+ */
 
 // handle displaying the current day
 function displayDay() {
@@ -19,15 +23,26 @@ function displayDay() {
 
 };
 
-/**
+
+/*
  * timeblocks
-*/
+ */
 
-// TESTING: Adding timeblocks
+// gets current hour as integer
+function getCurrentHour() {
+  currentHour = parseInt(moment().format("HH"), 10);
+  console.log("currentHour =", currentHour)
+}
 
 
-/**
+/*
  * function calls
-*/
+ */
 
 displayDay();
+getCurrentHour();
+
+
+/*
+ * TESTING
+ */
